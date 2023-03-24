69fc7cbdd00926c29ba38b9b4f6315effd13b4fc
CCotuna <calin.cotuna@emanuel.ro>
2023-03-24 08:38:30 +0200

    merging - v1

--- scripts.js
+++ scripts.js
@@ -1,26 +1,49 @@
 function setup() {
-    createCanvas(400,400)
+        createCanvas(400,400)
 }
 
 function draw () {
-background("#8a2be2")
-
-smileFaceCalin();
+    background("#a0daa9")
+    smileyFaceTimo(250, 200);
+    smileFaceCalin(120, 200);
 }
 
-function smileFaceCalin() {
+function smileyFaceTimo (x, y) {
+    //head
+    fill ("#99999")
+    circle(x, y+150, 30);
+    //eyes
+    fill ("#000000")
+    circle(x+5, y+146, 4);
+    circle(x-4, y+146, 4);
+    //pupiles
+
+    //mouth
+    fill ("#000000")
+    arc(x, y+152, 20, 20, 0, Math.PI);
+    
+    fill ("#fffff")
+    rect(x, y+152.5, 4, 4);
+    fill ("#fffff")
+    rect(x-4, y+152.5, 4, 4);
+    
+    //sprancene
+    //line(192, 343, 330,290, );
+}
+
+function smileFaceCalin(x, y) {
 //skin
 fill("#fe00f6");
-circle(200,360,35);
+circle(x,y+160,35);
 
 //right eye
-circle(205,357,4);
-circle(205,357,1);  //pupil
+circle(x+5,y+157,4);
+circle(x+5,y+157,1);  //pupil
 
 //left eye
-circle(195,357,4);
-circle(195,357,1);  //pupil
+circle(x-5,y+157,4);
+circle(x-5,y+157,1);  //pupil
 //mouth
 
-arc(200,364,12,10,0, Math.PI);
+arc(x,y+164,12,10,0, Math.PI);
 }
